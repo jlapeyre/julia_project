69c01a9784bba4e609b156bcf422b92fe251dc04
John Lapeyre <jlapeyre@users.noreply.github.com>
2021-12-09 09:37:17 -0500

    Polish a bit 	new file:   LICENSE 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+setuptools.setup(
     name='julia_project',
-    version='0.0.1',
+    version='0.0.2',
     description='Manage a Julia project inside a Python package',
-    # url='https://github.com/mypackage.git',
+    url='https://github.com/jlapeyre/julia_project.git',
     author='John Lapeyre',
-    # author_email='author@gmail.com',
-    packages=find_packages(),
+    packages=setuptools.find_packages(),
     py_modules=["julia_project", ],
-    install_requires=['julia', ] # numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+    install_requires=['julia', ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
 )
