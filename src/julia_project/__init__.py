9f18c1a111b821a0ec1f59956bc0189fb0081fb6
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-03-21 12:56:12 -0400

    Avoid possible bug if python is not on user path

--- src/julia_project/__init__.py
+++ src/julia_project/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1.21"
+__version__ = "0.1.22"
 
 import os
 import sys
@@ -9,7 +9,7 @@ from ._julia_project import JuliaProject
 os.environ["PYTHON_JULIACALL_NOINIT"] = "yes"
 os.environ["JULIA_PYTHONCALL_EXE"] = sys.executable or '' # When does the latter occur?
 os.environ['JULIA_PYTHONCALL_LIBPTR'] = str(ctypes.pythonapi._handle)
-os.environ['PYTHON'] = shutil.which("python")
+os.environ['PYTHON'] = shutil.which("python") or ''
 
 # This is only for debugging
 # import julia
